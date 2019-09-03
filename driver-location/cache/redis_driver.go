a941a5f817152f26546b64cc9bb28387e0492081
hosseio <hosseio82@gmail.com>
2019-09-03 22:16:39 +0200

    driver-location: redis implementation of driver view and driver repository. test incoming

---
+++ driver-location/cache/redis_driver.go
@@ -0,0 +1,118 @@
+package cache
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/heetch/jose-odg-technical-test/driver-location"
+
+	"github.com/gomodule/redigo/redis"
+	"github.com/heetch/jose-odg-technical-test/driver-location/internal"
+)
+
+const prefix = "heetch.driver-location"
+
+type RedisDriver struct {
+	client        redis.Pool
+	driverBuilder driver_location.DriverBuilder
+}
+
+func NewRedisDriver(address string, driverBuilder driver_location.DriverBuilder) RedisDriver {
+	pool := redis.Pool{
+		Dial: func() (conn redis.Conn, e error) {
+			return redis.Dial("tcp", address)
+		},
+		MaxIdle:   80,
+		MaxActive: 12000,
+	}
+
+	return RedisDriver{client: pool, driverBuilder: driverBuilder}
+}
+
+type LocationDTO struct {
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+	UpdatedAt string  `json:"updated_at"` // date in RFC3330 format
+}
+
+type DriverLocationDTO struct {
+	DriverID  string        `json:"driver_id"`
+	Locations []LocationDTO `json:"locations"`
+}
+
+func (rd RedisDriver) Save(driver domain.Driver) error {
+	conn := rd.client.Get()
+	defer conn.Close()
+
+	key := fmt.Sprintf("%s:%s", prefix, driver.DriverID().String())
+
+	driverLocationDTO := rd.entityToDTO(driver)
+	bytes, err := json.Marshal(driverLocationDTO)
+	if err != nil {
+		return err
+	}
+
+	_, err = conn.Do("SET", key, bytes)
+
+	return err
+}
+
+func (rd RedisDriver) ById(id string) (domain.Driver, error) {
+	conn := rd.client.Get()
+	defer conn.Close()
+
+	key := fmt.Sprintf("%s:%s", prefix, id)
+
+	bytes, err := redis.Bytes(conn.Do("GET", key))
+	if err != nil {
+		return domain.Driver{}, domain.ErrDriverNotFound
+	}
+
+	var driverLocationDTO DriverLocationDTO
+	err = json.Unmarshal(bytes, &driverLocationDTO)
+	if err != nil {
+		return domain.Driver{}, domain.ErrDriverNotFound
+	}
+
+	return rd.dtoToEntity(driverLocationDTO)
+}
+
+func (rd RedisDriver) entityToDTO(driver domain.Driver) DriverLocationDTO {
+	locationsDTO := make([]LocationDTO, 0)
+	for _, location := range driver.Locations() {
+		locationDTO := LocationDTO{
+			Latitude:  location.Latitude(),
+			Longitude: location.Longitude(),
+			UpdatedAt: location.UpdatedAt().String(),
+		}
+		locationsDTO = append(locationsDTO, locationDTO)
+	}
+
+	return DriverLocationDTO{
+		DriverID:  driver.DriverID().String(),
+		Locations: locationsDTO,
+	}
+}
+
+func (rd RedisDriver) dtoToEntity(dto DriverLocationDTO) (domain.Driver, error) {
+	locations := []driver_location.LocationBuilderDTO{}
+	for _, location := range dto.Locations {
+		updatedAt, err := time.Parse(time.RFC3339, location.UpdatedAt)
+		if err != nil {
+			return domain.Driver{}, err
+		}
+		locations = append(locations, driver_location.LocationBuilderDTO{
+			Latitude:  location.Latitude,
+			Longitude: location.Longitude,
+			UpdatedAt: updatedAt,
+		})
+	}
+
+	builderDTO := driver_location.DriverBuilderDTO{
+		DriverID:  dto.DriverID,
+		Locations: locations,
+	}
+
+	return rd.driverBuilder.Build(builderDTO)
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/bitly/go-nsq v1.0.7 // indirect
 	github.com/chiguirez/cromberbus v1.0.0
 	github.com/golang/snappy v0.0.1 // indirect
+	github.com/gomodule/redigo v2.0.0+incompatible
 	github.com/nsqio/go-nsq v1.0.7
 	github.com/satori/go.uuid v1.2.0
 	github.com/stretchr/testify v1.4.0
